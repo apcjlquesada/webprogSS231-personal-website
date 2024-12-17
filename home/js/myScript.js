a5343cce9190557754cf0e169a42d89076531579
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2024-12-17 08:37:36 +0800

    updated function2

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -5,9 +5,7 @@ function myFunction() {
 function myFunction2() {
     const element = document.getElementsByTagName("h1");
 
-    document.getElementById("demo").innerHTML = `The text in first h1 tag (index 0) is: ${element[0].innerHTML}`;
-
-
+    document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
 
 
 }
