3fd79971df359713e583807768eb3c87c70d613d
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2024-12-17 08:58:44 +0800

    update

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -3,7 +3,7 @@ function myFunction() {
 }
 
 function myFunction2() {
-    const element = document.getElementsByTagName("br");
+    const element = document.getElementsByTagName("p");
 
     
     document.getElementById("demo").innerHTML = '<br> The text in first paragraph (index 0) is: <br>' + element[0].innerHTML;
